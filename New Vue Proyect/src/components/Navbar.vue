53dedbc481225533c162736aebb18a80eeec79ba
Gaston Guido <gaston.guido88@gmail.com>
2024-06-22 13:57:34 +0200

    Primer Commit

---
+++ New Vue Proyect/src/components/Navbar.vue
@@ -0,0 +1,35 @@
+  <script>
+  export default {
+    data() {
+      return {
+        user: 'Admin'
+      };
+    }
+  };
+  </script>
+
+<template>
+    <nav class="navbar">
+      <img src="" alt="Logo" class="logo" />
+      <div class="user-info">
+        <span>{{ user }}</span>
+      </div>
+    </nav>
+  </template>
+  
+  <style scoped>
+  .navbar {
+    display: flex;
+    justify-content: space-between;
+    padding: 10px;
+    background-color: #333;
+    color: white;
+  }
+  .logo {
+    height: 40px;
+  }
+  .user-info {
+    align-self: center;
+  }
+  </style>
+  
